daa75bf61d158333e5949e0bb11894aefb4508f8
Lin, Yiu-Chang <yiuchang.lin@rakuten.com>
2018-06-25 10:51:31 -0400

    Adding final leaderboard js.

---
+++ final_leaderboard.js
@@ -0,0 +1,97 @@
+fetch("https://sigir-ecom-leaderboard.github.io/score_final.json")
+  .then(res => res.json())
+  .then((homework) => {console.log(homework);
+
+var d = new Date();
+var lastUpdated = d.toLocaleString();
+var scoreDiv = document.getElementById("finalscorediv");
+var updatedDiv = document.getElementById("finalupdatedDiv");
+var arrayToSort = new Array();
+
+function resort(sortIndex, ascending) {
+  arrayToSort = new Array();
+  for (var user in homework) {
+    var userArray = new Array();
+    userArray.push(user);
+    var scores = homework[user];
+    for (var sc in scores) {
+      userArray.push(scores[sc]);
+    }
+    arrayToSort.push(userArray);
+  }
+
+  arrayToSort.sort(function(a,b) {
+    var scoreA = a[sortIndex];
+    var scoreB = b[sortIndex];
+    if(scoreA == -1) {
+      if (ascending == 1) {
+        scoreA = 9999;
+      } else {
+        scoreA = -9999;
+      }
+    }
+    if(scoreB == -1){
+      if (ascending == 1) {
+        scoreB = 9999;
+      } else {
+        scoreB = -9999;
+      }
+    }
+    if(ascending == 0){
+      return scoreB - scoreA;
+    }
+    else {
+      return scoreA - scoreB;
+    }
+  });
+}
+
+// updatedDiv.innerHTML = "last updated at " + lastUpdated;
+updatedDiv.innerHTML = "&nbsp;"
+
+
+function drawLeaderboard() {
+  while (scoreDiv.firstChild) {
+    scoreDiv.removeChild(scoreDiv.firstChild);
+  }
+  var currentRank = 1;
+  for (var userIndex = 0; userIndex < arrayToSort.length; userIndex++) {
+      var userArray = arrayToSort[userIndex];
+      var user = userArray[0];
+      var state = "";
+      var trEl = document.createElement("tr");
+      scoreDiv.appendChild(trEl);
+      var rank = "";
+      if (user == "oracle" || user == "baseline" || user == "default"){
+         user = "<b>" + user + "</b>"
+      }
+      else {
+         rank = currentRank;
+         currentRank = currentRank+1;
+      }
+      var rankTd = document.createElement('td');
+      rankTd.innerHTML = rank;
+      trEl.appendChild(rankTd);
+
+      var stateTd = document.createElement('td');
+      stateTd.innerHTML = state;
+      trEl.appendChild(stateTd);
+
+      var userTd = document.createElement('td');
+      userTd.innerHTML = user;
+      trEl.appendChild(userTd);
+
+    for (var hwindex = 1; hwindex < userArray.length; hwindex++) {
+      var scTd = document.createElement('td');
+      var sc = userArray[hwindex];
+      if(sc != -1) {
+        scTd.innerHTML = sc;
+      }
+      trEl.appendChild(scTd)
+    }
+  }
+}
+
+resort(3, 0);
+drawLeaderboard();
+});
